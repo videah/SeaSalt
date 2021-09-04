0bdda78c236d0c7c08ba9aca333c16c93437ef84
videah <videah@selfish.systems>
2021-09-04 05:22:05 +0100

    add clear cache to settings, initial loading

--- lib/cubits/search_cubit.dart
+++ lib/cubits/search_cubit.dart
@@ -4,10 +4,12 @@ import 'package:seasalt/cubits/search_state.dart';
 import 'package:seasalt/repositories/search_repository.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-
   final SearchRepository repository;
 
-  SearchCubit({required this.repository}) : super(SearchInitial(tags: ""));
+  SearchCubit({required this.repository}) : super(SearchInitial(tags: "")) {
+    // Searching empty just gives us the "front page" with the newest posts.
+    search("");
+  }
 
   Future<void> search(String tags) async {
     // Show loading indicators and potentially store tags for later use.
@@ -16,6 +18,7 @@ class SearchCubit extends Cubit<SearchState> {
     // Pull posts using the provided tags.
     final posts = await repository.search(state.tags);
 
+    // Present posts to the the user if we got some, otherwise display an error.
     if (posts != null) {
       emit(SearchResult(tags: state.tags, posts: posts));
     } else {
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:seasalt/style.dart';
 import 'package:seasalt/widgets/theme_select_button.dart';
 
@@ -14,6 +15,8 @@ class SettingsPage extends StatelessWidget {
         body: ListView(
           children: [
             ThemeSelectTile(),
+            Divider(),
+            CacheManagementTile(),
           ],
         ),
       ),
@@ -35,17 +38,56 @@ class ThemeSelectTile extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: Wrap(
             children: [
-              for (var theme in appThemes.values) Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ThemeSelectButton(
-                  themeName: theme.name,
-                  theme: theme,
+              for (var theme in appThemes.values)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ThemeSelectButton(
+                    themeName: theme.name,
+                    theme: theme,
+                  ),
                 ),
-              ),
             ],
           ),
-        )
+        ),
       ],
+    );
+  }
+}
+
+class CacheManagementTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        "Clear Image Cache",
+        style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+      ),
+      onTap: () async {
+        //
+        try {
+          await DefaultCacheManager().emptyCache();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                "Successfully cleared image cache.",
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.green,
+            ),
+          );
+        } catch (e) {
+          print("Could not clear image cache: $e");
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                "Could not clear cache... this could be a bug, please report.",
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+      },
     );
   }
 }
